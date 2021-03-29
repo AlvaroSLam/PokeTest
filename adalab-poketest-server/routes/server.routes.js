9dee92874bc657e905f6b5336604fe06c6fd6346
AlvaroSL <alvaro.sanchez.lamadrid@gmail.com>
2021-03-29 11:18:52 +0200

    Server fetching Pokemon information with Promise.all

--- adalab-poketest-server/routes/server.routes.js
+++ adalab-poketest-server/routes/server.routes.js
@@ -5,24 +5,20 @@ const axios =  require('axios')
 router.get('/allPokemon', (req, res) => {
     axios.get('https://pokeapi.co/api/v2/pokemon/')
         .then((response) =>{
-            
-            let pokeArr = response.data.results
-            pokeArr.forEach((elem, index) =>{
-                let url =  elem.url                
-                axios.get(url)
-                .then((result) => {
-                    let newArr = []
-                    newArr.push(result.data)
-                    console.log('Here starts -------------------------------------------')
-                    console.log(newArr)
-                    console.log('Here ends -------------------------------------------')
-                })
-                .catch(err => console.log(err))
-            })        
-            
+            let pokeArr =  response.data.results
+            let promises =  pokeArr.map((elem) =>{
+                return axios.get(elem.url)  
+            })
+           Promise.all(promises)
+              .then((allResults) => {
+                   res.json(allResults)
+               })
+               .catch((err) =>{
+                    console.log('Error while fetching', err)
+               })
         })
-        .catch((error) =>{
-            console.log(error)
+        .catch((err) =>{
+            console.log('Error while fetching', err)
         })
   })
 
